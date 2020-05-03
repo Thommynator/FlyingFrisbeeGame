624b6828754b556be3591e7d7604579e0c4b06db
Thomas <Thommynator@users.noreply.github.com>
2020-05-03 19:09:43 +0200

    #5 select player (#14)
    * Players can be selected by clicking on them.
    
    Selecting them:
    * changes their color
    * enables movement
    
    All other players are unselected, i.e. they can't move anymore.
    The Player holding the frisbee can not be selected.
    
    * Possibility to cancel aiming
    
    The aiming is canceled when the distance is not long enough.
    This means if you aim and move the mouse back to the starting point, the frisbee won't be thrown.

---
+++ Flying Frisbee Game/Assets/Scripts/PlayerSelector.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSelector : MonoBehaviour
+{
+
+    /// layer used for raycast detection
+    public LayerMask layerMask;
+
+    public Material playerMaterial;
+    public Material selectedPlayerMaterial;
+
+    private GameObject frisbee;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        frisbee = GameObject.FindGameObjectWithTag("Frisbee");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity, layerMask))
+            {
+                if (hit.transform.gameObject.tag == "Player")
+                {
+                    GameObject hitPlayer = hit.transform.gameObject;
+                    Debug.Log("Hit Player: " + hitPlayer.name);
+                    DeselectAllPlayers();
+                    SelectPlayer(hitPlayer);
+                }
+            }
+        }
+    }
+
+    /// Sets the "canMove" property of all players to false and assigns the default player material to them.
+    private void DeselectAllPlayers()
+    {
+        foreach (Transform child in transform)
+        {
+            child.gameObject.GetComponent<PlayerMovement>().canMove = false;
+            child.gameObject.GetComponent<MeshRenderer>().material = playerMaterial;
+        }
+    }
+
+    /// Selects a given player, i.e. allow movement and assign different color
+    private void SelectPlayer(GameObject player)
+    {
+        if (player != frisbee.GetComponent<Frisbee>().GetPlayerHoldingFrisbee())
+        {
+            player.GetComponent<PlayerMovement>().canMove = true;
+            player.GetComponent<MeshRenderer>().material = selectedPlayerMaterial;
+        }
+    }
+}
